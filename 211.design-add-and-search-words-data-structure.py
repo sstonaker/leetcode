7addbe7a7900a9d93a589059207f45e14692ea84
Steven <52871164+sstonaker@users.noreply.github.com>
2023-03-19 09:33:49 -0500

    3-19-2023

---
+++ 211.design-add-and-search-words-data-structure.py
@@ -0,0 +1,40 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for ch in word:
+            cur = cur.children.setdefault(ch, TrieNode())
+        cur.end = True
+
+    def search(self, word: str) -> bool:
+        def dfs(node, index):
+            if index == len(word):
+                return node.end
+
+            if word[index] == ".":
+                for child in node.children.values():
+                    if dfs(child, index+1):
+                        return True
+
+            if word[index] in node.children:
+                return dfs(node.children[word[index]], index+1)
+
+            return False
+
+        return dfs(self.root, 0)
+
+
+class TrieNode:
+
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
