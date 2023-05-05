c2ef2c72d31a2ebb604bc4b92b5f2615d2417b0e
Steven <52871164+sstonaker@users.noreply.github.com>
2023-05-05 15:24:01 -0500

    30-day-js

---
+++ 2667.create-hello-world-function.js
@@ -0,0 +1,13 @@
+/**
+ * @return {Function}
+ */
+const createHelloWorld = () => {
+  return (...args) => {
+    return "Hello World";
+  };
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
