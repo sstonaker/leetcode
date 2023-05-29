f81cc7b6c861698775a248b6149f83574f17c1e9
Steven <52871164+sstonaker@users.noreply.github.com>
2023-05-29 07:06:37 -0500

    5-29-2023

--- 1603.design-parking-system.py
+++ 1603.design-parking-system.py
@@ -1,10 +1,10 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        self.spaces = {"1": big, "2": medium, "3": small}
+        self.spaces = {1: big, 2: medium, 3: small}
 
     def addCar(self, carType: int) -> bool:
-        if self.spaces[str(carType)] > 0:
-            self.spaces[str(carType)] -= 1
+        if self.spaces[carType] > 0:
+            self.spaces[carType] -= 1
             return True
         return False
